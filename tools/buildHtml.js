214a504747e3f19e4c849fc180957632c52bc747
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-11 16:35:37 -0700

    Fixed pertinent linting errors/warnings

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -2,18 +2,18 @@ import fs from 'fs';
 import cheerio from 'cheerio';
 import colors from 'colors';
 
-/*eslist-disable no-console*/
+/* eslist-disable no-console */
 
-fs.readFile('src/index.html', 'utf8', (err, markup) =>{
-  if(err){
+fs.readFile('src/index.html', 'utf8', (err, markup) => {
+  if (err) {
     return console.log(err);
   }
   const $ = cheerio.load(markup);
 
   $('head').prepend('<link rel="stylesheet" href="style.css">');
-  fs.writeFile('dist/index.html', $.html(), 'utf8', function(err){
-    if(err){
-      return console.log(err);
+  fs.writeFile('dist/index.html', $.html(), 'utf8', error => {
+    if (error) {
+      return console.log(error);
     }
     console.log('index.html written to /dist'.green);
   });
