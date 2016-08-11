33c99b801220cacaf8028e085f24dfe694b0e820
Nikhil Ram <nikhilram00@gmail.com>
2016-08-11 15:51:12 -0700

    header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,18 +1,46 @@
-import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
-import LoadingDots from './LoadingDots';
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/authorActions';
 
-const Header = ({ loading }) =>
-  <nav>
-    <IndexLink to="/" activeClassName="active">Home</IndexLink>
-    {loading && <LoadingDots interval={100} dots={20} />}
-    {" | "}
-    <Link to="/" activeClassName="active">Another Link</Link>
-  </nav>;
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Enter a Search Term'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this); 
+  }
 
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+  
+
+  handleClick() {
+    // loadArticles(this.state.value);
+    // just alerting for now, but the intention is to plug input value into API action
+    alert(this.state.value);
+  }
+
+
+  render() {
+    return (
+      <div style={{float: "right"}}>
+        <input placeholder='Enter a Topic' onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Get Feels</button>
+      </div>
+    );
+  }
+}
 Header.propTypes = {
-  loading: PropTypes.bool.isRequired,
+
+};
+Header.defaultProps = {
+
 };
 
 export default Header;
-
