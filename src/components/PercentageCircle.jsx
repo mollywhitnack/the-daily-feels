d5d119f9feb8765aed64bb79298c12a9b18de307
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-11 16:08:04 -0700

    FaceBoard and PercentageCircle components

---
+++ src/components/PercentageCircle.jsx
@@ -0,0 +1,9 @@
+import React, {PropTypes} from 'react';
+import Face from './Face';
+
+const PercentageCircle = face =>
+<div className="container-fluid">
+  <div className="circle text-center">
+    <h4>face.percentage</h4>
+  </div>
+</div>;
