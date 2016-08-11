1f526b21b403aada7fe967070683564733403fa9
mollywhitnack <mollywhitnack@gmail.com>
2016-08-11 15:09:22 -0700

    production take 2

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -1,13 +1,5 @@
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import rootReducer from '../reducers';
-
-export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
-  );
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./configureStore.prod');
+} else{
+  module.exports = require('./configureStore.dev');
 }
-
