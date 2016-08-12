04014dfdf82854aaee8141511e9dd7df9176cca9
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-12 13:48:58 -0700

    Content Page skeleton

--- src/components/content/FaceBoard.jsx
+++ src/components/content/FaceBoard.jsx
@@ -1,36 +1,8 @@
 import React, {PropTypes} from 'react';
 import Face from './Face';
 
-const faces = [
-  {
-    "img": "😠",
-    "emotion": "anger",
-    "percentage": 84,
-  },
-  {
-    "img": "😷",
-    "emotion": "disgust",
-    "percentage": 37,
-  },
-  {
-    "img": "😨",
-    "emotion": "fear",
-    "percentage": 25,
-  },
-  {
-    "img": "😄",
-    "emotion": "joy",
-    "percentage": 92,
-  },
-  {
-    "img": "😭",
-    "emotion": "sadness",
-    "percentage": 4,
-  }
-];
-
-const FaceBoard = faces =>
-<div className='container-fluid text-center'>
+const FaceBoard = ({faces}) =>
+<div className='container-fluid'>
     {faces.map(face =>
       <Face key={face.img} face={face} />
     )}
