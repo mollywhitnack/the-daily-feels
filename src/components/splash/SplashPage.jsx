2836982397f546e4293f34137237f486c8781e3c
mollywhitnack <mollywhitnack@gmail.com>
2016-08-11 17:07:33 -0700

    search bar in splash page

--- src/components/splash/SplashPage.jsx
+++ src/components/splash/SplashPage.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import TextInput from '../common/TextInput';
+import {Link } from 'react-router'
 
-const SplashPage = () =>
-  <div className="jumbotron">
-    <h1>The Daily Feels</h1>
-    <p>something something</p>
-  </div>;
+//const SplashPage = ({onChange, errors, onSave}) =>
+class SplashPage extends React.Component{
+  constructor(props, context){
+    super(props, context);
+
+    this.state = {
+      search: '',
+    };
+
+  this.updateSearchState = this.updateSearchState.bind(this);
+
+  }
+
+  updateSearchState(event){
+    const field = event.target.name;
+    let search = this.state.course;
+    search[field] = event.target.value;
+    return this.setState({search: course});
+  }
+
+  render(){
+    return(
+      <div className="jumbotron">
+      <h1>The Daily Feels</h1>
+      <p>something something</p>
+      <TextInput
+        name = "searchBy"
+        label = "Search News"
+        onChange = {this.updateSearchState} />
+
+      <Link to = {'/articles/' +this.state.search}
+           className = "btn btn-primary"> Submit </Link>
+
+      </div>
+    );
+  }
+}
 
 export default SplashPage;
