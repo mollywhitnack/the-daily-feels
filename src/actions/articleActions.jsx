ad9258d98e79cd4c0dfdebf4785ea80c8ebd481c
Matthew Broatch <mbroatch@gmail.com>
2016-08-12 15:03:46 -0700

    lint

--- src/actions/articleActions.jsx
+++ src/actions/articleActions.jsx
@@ -1,7 +1,6 @@
-import mockNewsApi from '../api/mockNewsApi';
 import * as types from './actionTypes';
 import { beginAjaxCall } from './ajaxStatusActions';
-import {get} from 'jquery';
+import { get } from 'jquery';
 
 export function loadArticlesSuccess(articles) {
   return { type: types.LOAD_ARTICLES_SUCCESS, articles };
--- src/reducers/articleReducer.jsx
+++ src/reducers/articleReducer.jsx
@@ -1,18 +1,13 @@
-// doesnt need name, could jsut export state
-//setting empty array means start with no articles
+//  doesnt need name, could just export state
+//  setting empty array means start with no articles
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-export default function articleReducer(state = initialState.articles, action){
-
-  switch(action.type){
-
+export default function articleReducer(state = initialState.articles, action) {
+  switch (action.type) {
     case types.LOAD_ARTICLES_SUCCESS:
-      console.log('action.articles:',action.articles )
       return action.articles;
-
-    default: 
+    default:
       return state;
   }
-
 }
