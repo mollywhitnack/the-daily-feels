6f1bae7c003275cab3eb67dd9d6d7d5eae34ae56
Matthew Broatch <mbroatch@gmail.com>
2016-08-12 15:12:18 -0700

    real news api plugged in

--- src/actions/articleActions.jsx
+++ src/actions/articleActions.jsx
@@ -2,6 +2,11 @@ import * as types from './actionTypes';
 import { beginAjaxCall } from './ajaxStatusActions';
 import { get } from 'jquery';
 
+
+const mockUrl = `/api/articles/${searchTerm}`
+const realUrl = `https://gateway-a.watsonplatform.net/calls/data/GetNews?apikey=339b0fd1a3561f37566508b88677559f27c553df&outputMode=json&start=now-1d&end=now&q.enriched.url.title=${searchTerm}&return=enriched.url.text,enriched.url.title,original.url`
+
+
 export function loadArticlesSuccess(articles) {
   return { type: types.LOAD_ARTICLES_SUCCESS, articles };
 }
@@ -9,7 +14,7 @@ export function loadArticlesSuccess(articles) {
 export function loadArticles(searchTerm) {
   return dispatch => {
     dispatch(beginAjaxCall());
-    return get(`/api/articles/${searchTerm}`)
+    return get(realUrl)
       .done(articles => {
         dispatch(loadArticlesSuccess(articles));
       })
