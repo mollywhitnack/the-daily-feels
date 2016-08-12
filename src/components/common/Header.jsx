afc3adbbc9c639f160f19b45dafc8204508122c9
Matthew Broatch <mnbroatch@gmail.com>
2016-08-11 17:44:04 -0700

    fix merge messup

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,6 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import * as courseActions from '../../actions/authorActions';
 
 class Header extends React.Component {
   constructor(props) {
