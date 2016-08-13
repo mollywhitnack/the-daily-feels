31cbf5a2e57fd6fc42b4ab759dd445a78729325f
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-12 17:42:18 -0700

    Pair ArticleList, Article, tone percentages - content page

--- src/components/content/FaceBoard.jsx
+++ src/components/content/FaceBoard.jsx
@@ -1,13 +1,17 @@
 import React, {PropTypes} from 'react';
 import Face from './Face';
 
-const FaceBoard = ({faces}) =>
-<div className='container-fluid'>
-    {faces.map(face =>
-      <Face key={face.img} face={face} />
-    )}
-</div>;
+const FaceBoard = ({faces}) => {
+  let windowUrl = window.location.href.split('/');
+  let searchTerm = windowUrl[4];
+  return (
+    <div className='container-fluid'>
+      {faces.map(face =>
+        <Face key={face.img} face={face} searchTerm={searchTerm} />
+
+      )}
+    </div>
+  )
+}
 
 export default FaceBoard;
-
-// <PercentageCircle key={face.img} face={face} />
