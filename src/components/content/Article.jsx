d23a06ec029dbc97aa5a0dbae3898fed1dd0cc36
mollywhitnack <mollywhitnack@gmail.com>
2016-08-12 20:36:24 -0700

    sort by emotion working-ish, current threshhold at 0.5

--- src/components/content/Article.jsx
+++ src/components/content/Article.jsx
@@ -2,20 +2,21 @@ import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
 import styles from '../../styles/styles.css';
 
-const Article = ({article, emotion}) => {
-
-  return (
-    <Link to={article.url} target="_blank">
-      <div className="col-xs-3 article">
-        <div>
-          {article.title}
+const Article = ({article}) => {
+  //console.log('emotion in article: ', emotion);
+  //console.log('article tone', article.tone);
+    return (
+      <Link to={article.url} target="_blank">
+        <div className="col-xs-3 article">
+          <div>
+            {article.title}
+          </div>
+          <div>
+            { `${article.snippet} ... ` }
+          </div>
         </div>
-        <div>
-          { `${article.snippet} ... ` }
-        </div>
-      </div>
-    </Link>
-  );
+      </Link>
+    );
 };
 
 Article.propTypes = {
