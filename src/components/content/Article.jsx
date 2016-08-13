0fe4fa548dddc4989ef85bf76c12337607d2749a
mollywhitnack <mollywhitnack@gmail.com>
2016-08-12 20:44:16 -0700

    sort and some linting

--- src/components/content/Article.jsx
+++ src/components/content/Article.jsx
@@ -1,26 +1,24 @@
-import React, {PropTypes} from 'react';
-import {Link} from 'react-router';
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 import styles from '../../styles/styles.css';
 
-const Article = ({article}) => {
-  //console.log('emotion in article: ', emotion);
-  //console.log('article tone', article.tone);
-    return (
-      <Link to={article.url} target="_blank">
-        <div className="col-xs-3 article">
-          <div>
-            {article.title}
-          </div>
-          <div>
-            { `${article.snippet} ... ` }
-          </div>
+const Article = ({ article }) => {
+  return (
+    <Link to={article.url} target="_blank">
+      <div className="col-xs-3 article">
+        <div>
+          {article.title}
         </div>
-      </Link>
-    );
+        <div>
+          {`${article.snippet} ... `}
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 Article.propTypes = {
-  article: PropTypes.object.isRequired
+  article: PropTypes.object.isRequired,
 };
 
 export default Article;
