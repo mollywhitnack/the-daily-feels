bd9e3197c56c06d5015f6d3881d789d1ffe9c53b
mollywhitnack <mollywhitnack@gmail.com>
2016-08-12 20:36:24 -0700

    sort by emotion working-ish, current threshhold at 0.5

--- src/components/content/ArticleList.jsx
+++ src/components/content/ArticleList.jsx
@@ -2,15 +2,35 @@ import React, {PropTypes} from 'react';
 import Article from './Article';
 
 const ArticleList = ({articles, emotion}) => {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        {articles.map(article =>
-          <Article key={article.id} article={article} emotion = {emotion}/>
-        )}
+
+  //console.log('articles.tone', articles);
+  console.log('emotion', emotion);
+
+  if(emotion){
+    return(
+      <div className="container-fluid">
+        <div className="row">
+          {articles.map(article =>{
+            for(let i =0; i<article.tone.length; i++){
+              if(article.tone[i].tone_id === emotion && article.tone[i].score > 0.5)
+                  return <Article key={article.id} article={article}/>
+            }
+          })}
+        </div>
       </div>
-    </div>
-  );
+    )
+  } 
+  else{
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          {articles.map(article =>
+            <Article key={article.id} article={article}/>
+          )}
+        </div>
+      </div>
+    );
+  }
 };
 
 ArticleList.propTypes = {
