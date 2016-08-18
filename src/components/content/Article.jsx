a78de1fa00bf909f2b67436fac95b6e465e6e547
Nikhil Ram <nikhilram00@gmail.com>
2016-08-17 19:34:37 -0700

    STDs and threshold arr

--- src/components/content/Article.jsx
+++ src/components/content/Article.jsx
@@ -15,9 +15,9 @@ const Article = ({ article }) =>
         <div className="back">
           <p className="articleEmotionRow anger"> anger: {(article.tone[0].score * 100).toFixed(1)}%</p>
           <p className="articleEmotionRow disgust"> disgust: {(article.tone[1].score * 100).toFixed(1)}%</p>
-          <p className="articleEmotionRow fear"> fear: {(article.tone[2].score).toFixed(1)}%</p>
-          <p className="articleEmotionRow joy"> joy: {(article.tone[3].score).toFixed(1)}%</p>
-          <p className="articleEmotionRow sadness"> sadness: {(article.tone[4].score).toFixed(1)}%</p>
+          <p className="articleEmotionRow fear"> fear: {(article.tone[2].score * 100).toFixed(1)}%</p>
+          <p className="articleEmotionRow joy"> joy: {(article.tone[3].score * 100).toFixed(1)}%</p>
+          <p className="articleEmotionRow sadness"> sadness: {(article.tone[4].score * 100).toFixed(1)}%</p>
         </div>
       </div>
     </a>
