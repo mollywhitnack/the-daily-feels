3ebf1223c79358714f212863da2eec5d0a5712d9
Nikhil Ram <nikhilram00@gmail.com>
2016-08-17 21:30:15 -0700

    changed dominantColor to Tone and refactored percentages to values

--- src/components/content/Article.jsx
+++ src/components/content/Article.jsx
@@ -13,11 +13,11 @@ const Article = ({ article }) =>
           <div className="articleSnippet">{`${article.snippet} ... `}</div>
         </div>
         <div className="back">
-          <p className="articleEmotionRow anger"> anger: {(article.tone[0].score * 100).toFixed(1)}%</p>
-          <p className="articleEmotionRow disgust"> disgust: {(article.tone[1].score * 100).toFixed(1)}%</p>
-          <p className="articleEmotionRow fear"> fear: {(article.tone[2].score * 100).toFixed(1)}%</p>
-          <p className="articleEmotionRow joy"> joy: {(article.tone[3].score * 100).toFixed(1)}%</p>
-          <p className="articleEmotionRow sadness"> sadness: {(article.tone[4].score * 100).toFixed(1)}%</p>
+          <p className="articleEmotionRow anger"> anger: {(article.tone[0].std.toFixed(1))}</p>
+          <p className="articleEmotionRow disgust"> disgust: {(article.tone[1].std.toFixed(1))}</p>
+          <p className="articleEmotionRow fear"> fear: {(article.tone[2].std.toFixed(1))}</p>
+          <p className="articleEmotionRow joy"> joy: {(article.tone[3].std.toFixed(1))}</p>
+          <p className="articleEmotionRow sadness"> sadness: {(article.tone[4].std.toFixed(1))}</p>
         </div>
       </div>
     </a>
