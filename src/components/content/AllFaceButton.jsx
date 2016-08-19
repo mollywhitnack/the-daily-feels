73bcc45c132e896fa9be2c7faceed32b47ad0d1c
Matthew Broatch <mbroatch@gmail.com>
2016-08-18 23:23:49 -0700

    all button tweaks

--- src/components/content/AllFaceButton.jsx
+++ src/components/content/AllFaceButton.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router';
 import '../../styles/AllFaceButton.scss';
 
 const AllFaceButton = () =>
-  <div className="inline allEmotions">
+  <div className="inline allEmotions faceAlign">
     <div className="emotionLabel">All</div>
     <div className="allFaces">
-      <div className="smallFace emj" style={{top:0, left:-1, zIndex:2, }}>😄</div>
-      <div className="smallFace emj" style={{top:25, left:-18, zIndex:1, }}>😠</div>
-      <div className="smallFace emj" style={{top:28, left:16, zIndex:3, }}>😭</div>
+      <div className="smallFace emj" style={{top:12, left:-3, zIndex:2, }}>😄</div>
+      <div className="smallFace emj" style={{top:45, left:-24, zIndex:1, }}>😠</div>
+      <div className="smallFace emj" style={{top:48, left:18, zIndex:3, }}>😭</div>
     </div>
   </div>;
 
