a2ba86a9c37434fb1d10b6401d12c911f9ce36b5
Matthew Broatch <mbroatch@gmail.com>
2016-08-19 01:17:15 -0700

    content page styles, full lint

--- src/components/content/AllFaceButton.jsx
+++ src/components/content/AllFaceButton.jsx
@@ -1,15 +1,22 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
-import '../../styles/AllFaceButton.scss';
+import '../../styles/allFaceButton.scss';
+import '../../styles/face.scss';
 
-const AllFaceButton = () =>
-  <div className="inline allEmotions faceAlign">
+const AllFaceButton = ({ searchTerm }) =>
+  <div className="allEmotions faceAlign">
     <div className="emotionLabel">All</div>
-    <div className="allFaces">
-      <div className="smallFace emj" style={{top:12, left:-3, zIndex:2, }}>😄</div>
-      <div className="smallFace emj" style={{top:45, left:-24, zIndex:1, }}>😠</div>
-      <div className="smallFace emj" style={{top:48, left:18, zIndex:3, }}>😭</div>
-    </div>
+    <Link to={`/articles/${searchTerm}`}>
+      <div className="allFaces">
+        <div className="smallFace emj" style={{ top: 15, left: 17, zIndex: 2 }}>😄</div>
+        <div className="smallFace emj" style={{ top: 48, left: -4, zIndex: 1 }}>😠</div>
+        <div className="smallFace emj" style={{ top: 51, left: 38, zIndex: 3 }}>😭</div>
+      </div>
+    </Link>
   </div>;
+
+AllFaceButton.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+};
 
 export default AllFaceButton;
--- src/components/content/Article.jsx
+++ src/components/content/Article.jsx
@@ -1,47 +1,62 @@
 import React, { PropTypes } from 'react';
 import '../../styles/article.scss';
 
-/*let dominantStyle = {
+/*  let dominantStyle = {
 
-}*/
+}  */
 
 const Article = ({ article }) => {
+  const dominantToneShadowObj = {
+    anger: 'red',
+    disgust: 'green',
+    fear: '#6600CC',
+    joy: '#FF00FF',
+    sadness: '#0099FF',
+  };
+
 
   let dominantTone = {
     backgroundColor: 'white',
-  }
-  if (article.dominantTone === 'anger') {
-    dominantTone = {
-      //backgroundColor: 'red',
-      boxShadow: 'inset 0 0 10px red'
-    }}
-  if(article.dominantTone === 'disgust'){dominantTone = {
-    boxShadow: '3px 3px 7px 3px #999, inset 0px 0px 10px green',
-  }}
-  if(article.dominantTone === 'fear'){dominantTone = {
-    boxShadow: '3px 3px 7px 3px #999, inset 0px 0px 10px #6600cc',
+  };
 
-  }}
-  if(article.dominantTone === 'joy'){dominantTone = {
-    boxShadow: '3px 3px 7px 3px #999, inset 0px 0px 10px #ff00ff',
-  }}
-  if(article.dominantTone === 'sadness'){dominantTone = {
-    boxShadow: '3px 3px 7px 3px #999, inset 0px 0px 10px #0099ff',
-  }}
+  dominantTone = {
+    boxShadow: `3px 3px 7px 3px #999,
+     inset 0px 0px 10px ${dominantToneShadowObj[article.dominantTone]}`,
+  };
+
 
   return (
     <div className="article">
       <a href={article.url} target="_blank" className="flip-container">
         <div className="flipper">
-          <div className="front" style = {dominantTone} >
-            <div className="articleTitle"  >{article.title}</div>
+          <div className="front" style={dominantTone} >
+            <div className="articleTitle">{article.title}</div>
             <div className="articleSnippet">{`${article.snippet} ... `}</div>
+          </div>
+          <div className="back">
+
+            <p className="articleEmotionRow anger">
+              anger: {(article.tone[0].std.toFixed(1))}
+            </p>
+            <p className="articleEmotionRow disgust">
+              disgust: {(article.tone[1].std.toFixed(1))}
+            </p>
+            <p className="articleEmotionRow fear">
+              fear: {(article.tone[2].std.toFixed(1))}
+            </p>
+            <p className="articleEmotionRow joy">
+              joy: {(article.tone[3].std.toFixed(1))}
+            </p>
+            <p className="articleEmotionRow sadness">
+              sadness: {(article.tone[4].std.toFixed(1))}
+            </p>
+
           </div>
         </div>
       </a>
     </div>
-  )
-}
+  );
+};
 
 Article.propTypes = {
   article: PropTypes.object.isRequired,
