32270f8057b9822d539a147e796b7b5d95008347
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-19 16:57:22 -0700

    All Emotions button active color and hover animation

--- src/components/content/AllFaceButton.jsx
+++ src/components/content/AllFaceButton.jsx
@@ -3,17 +3,21 @@ import { Link } from 'react-router';
 import '../../styles/allFaceButton.scss';
 import '../../styles/face.scss';
 
-const AllFaceButton = ({ searchTerm }) =>
-  <div className="allEmotions faceAlign">
-    <div className="emotionLabel">All</div>
-    <Link to={`/articles/${searchTerm}`}>
-      <div className="allFaces">
-        <div className="smallFace emj" id="joy" style={{ top: 15, left: 17, zIndex: 2 }}>😄</div>
-        <div className="smallFace emj" id="anger" style={{ top: 48, left: -4, zIndex: 1 }}>😠</div>
-        <div className="smallFace emj" id="sadness" style={{ top: 51, left: 38, zIndex: 3 }}>😭</div>
-      </div>
-    </Link>
-  </div>;
+const AllFaceButton = ({ searchTerm, currentEmotion }) => {
+  console.log('currentEmotion:', currentEmotion);
+  return (
+    <div className="allEmotions faceAlign">
+      <div className="emotionLabel">All</div>
+      <Link to={`/articles/${searchTerm}`}>
+        <div className="allFaces">
+          <div className="smallFace emj" id={currentEmotion ? "" : "joyActive"} style={{ top: 15, left: 17, zIndex: 2 }}>😄</div>
+          <div className="smallFace emj" id={currentEmotion ? "" : "angerActive"} style={{ top: 48, left: -4, zIndex: 1 }}>😠</div>
+          <div className="smallFace emj" id={currentEmotion ? "" : "sadnessActive"} style={{ top: 51, left: 38, zIndex: 3 }}>😭</div>
+        </div>
+      </Link>
+    </div>
+  )
+}
 
 AllFaceButton.propTypes = {
   searchTerm: PropTypes.string.isRequired,
