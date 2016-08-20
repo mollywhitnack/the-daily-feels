882898e1681b407f862ff0c7dd6dac4e1c9e9d24
Matthew Broatch <mbroatch@gmail.com>
2016-08-19 18:05:38 -0700

    refactored article

---
+++ src/components/content/ArticleDescriptionButton.jsx
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
+import '../../styles/article.scss';
+
+const ArticleDescriptionButton = () => {
+
+  return (
+    <div className="descriptionButton">
+      ?
+    </div>
+  );
+};
+
+export default ArticleDescriptionButton;
+
