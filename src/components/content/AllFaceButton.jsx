8fec23935af5192855c914589a9f950771c2af52
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-19 21:21:17 -0700

    All Buttons hover back to working

--- src/components/content/AllFaceButton.jsx
+++ src/components/content/AllFaceButton.jsx
@@ -3,22 +3,28 @@ import { Link } from 'react-router';
 import '../../styles/allFaceButton.scss';
 import '../../styles/face.scss';
 
-const AllFaceButton = ({ searchTerm }) =>
+const AllFaceButton = ({ searchTerm, currentEmotion }) =>
   <div className="allEmotions faceAlign">
     <div className="emotionLabel">All</div>
     <Link to={`/articles/${searchTerm}`}>
       <div className="allFaces">
-        <div className="smallFace emj" id="joy" style={{ top: 15, left: 17, zIndex: 2 }}>😄</div>
         <div
           className="smallFace emj"
-          id="anger"
+          id={currentEmotion ? "joyHover" : "joyActive"}
+          style={{ top: 15, left: 17, zIndex: 2 }}
+        >
+          😄
+        </div>
+        <div
+          className="smallFace emj"
+          id={currentEmotion ? "angerHover" : "angerActive"}
           style={{ top: 48, left: -4, zIndex: 1 }}
         >
           😠
         </div>
         <div
           className="smallFace emj"
-          id="sadness"
+          id={currentEmotion ? "sadnessHover" : "sadnessActive"}
           style={{ top: 51, left: 38, zIndex: 3 }}
         >
           😭
@@ -29,6 +35,7 @@ const AllFaceButton = ({ searchTerm }) =>
 
 AllFaceButton.propTypes = {
   searchTerm: PropTypes.string.isRequired,
+  currentEmotion: PropTypes.string,
 };
 
 export default AllFaceButton;
