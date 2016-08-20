edf5a8afa13354cb5edf23d18ca0b024b1b69701
Sonam Dolker Kindy <sonam.d.kindy@gmail.com>
2016-08-19 19:54:35 -0700

    Linted Face comp and emotionColorKey

--- src/emotionColorKey.js
+++ src/emotionColorKey.js
@@ -1,9 +1,9 @@
-let emotionColorKey = {
+const emotionColorKey = {
   anger: '#ff6961',
   disgust: '#77dd77',
   fear: '#b19cd9',
   joy: '#fdfd96',
   sadness: '#C1F0F6',
-}
+};
 
 export default emotionColorKey;
